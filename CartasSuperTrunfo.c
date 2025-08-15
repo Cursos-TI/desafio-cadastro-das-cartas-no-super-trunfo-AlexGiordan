611e776086d1fa651384007079935ee38abead8a
AlexGiordan <alex.giordan@hotmail.com>
2025-08-15 00:29:42 +0000

    Ajustando a impressaõ do resultado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,13 @@ typedef struct {
     double SuperPoder;
 } Carta;
 
+typedef struct {
+    const char *Carta;
+    double ValueUm;
+    double ValueDois;
+    int MenorValue;
+} Comparacao;
+
 void limpar_nova_linha(char *str) {
     str[strcspn(str, "\n")] = '\0';
 }
@@ -162,47 +169,27 @@ int main() {
     printf("Finalizando Cadastro Super Trunfo\n\n");
     printf("Comparação de cartas:\n");
 
-    if(Baralho[0].Populacao >= Baralho[1].Populacao){
-        printf("População: Carta 1 venceu (1)\n");
-    } else {
-        printf("População: Carta 2 venceu (0)\n");
-    }
+    Comparacao PontosAtributos[] = {
+        { "População",             (double)Baralho[0].Populacao,              (double)Baralho[1].Populacao,              0 },
+        { "Área",                  (double)Baralho[0].Area,                   (double)Baralho[1].Area,                   0 },
+        { "PIB",                   (double)Baralho[0].PIB,                    (double)Baralho[1].PIB,                    0 },
+        { "Pontos Turísticos",     (double)Baralho[0].NumeroPontosTuristicos, (double)Baralho[1].NumeroPontosTuristicos, 0 },
+        { "Densidade Populacional",(double)Baralho[0].DensidadePopulacional,  (double)Baralho[1].DensidadePopulacional,  1 },
+        { "PIB per Capita",        (double)Baralho[0].PIBPerCapita,           (double)Baralho[1].PIBPerCapita,           0 },
+        { "Super Poder",           (double)Baralho[0].SuperPoder,             (double)Baralho[1].SuperPoder,             0 }
+    };
 
-    if(Baralho[0].Area >= Baralho[1].Area){
-        printf("Área: Carta 1 venceu (1)\n");
-    } else {
-        printf("Área: Carta 2 venceu (0)\n");
-    }
+    size_t total = sizeof(PontosAtributos) / sizeof(PontosAtributos[0]);
 
-    if(Baralho[0].PIB >= Baralho[1].PIB){
-        printf("PIB: Carta 1 venceu (1)\n");
-    } else {
-        printf("PIB: Carta 2 venceu (0)\n");
-    }
+    for (size_t i = 0; i < total; i++) {
+        int resultado;
+        if (PontosAtributos[i].MenorValue)
+            resultado = (PontosAtributos[i].ValueUm <= PontosAtributos[i].ValueDois) ? 1 : 0;
+        else
+            resultado = (PontosAtributos[i].ValueUm >= PontosAtributos[i].ValueDois) ? 1 : 0;
 
-    if(Baralho[0].NumeroPontosTuristicos >= Baralho[1].NumeroPontosTuristicos){
-        printf("Pontos Turísticos: Carta 1 venceu (1)\n");
-    } else {
-        printf("Pontos Turísticos: Carta 2 venceu (0)\n");
+        printf("%s: %d\n", PontosAtributos[i].Carta, resultado);
     }
-
-    if(Baralho[0].DensidadePopulacional <= Baralho[1].DensidadePopulacional){
-        printf("Densidade Populacional: Carta 1 venceu (1)\n");
-    } else {
-        printf("Densidade Populacional: Carta 2 venceu (0)\n");
-    }
-
-    if(Baralho[0].PIBPerCapita >= Baralho[1].PIBPerCapita){
-        printf("PIB per Capita: Carta 1 venceu (1)\n");
-    } else {
-        printf("PIB per Capita: Carta 2 venceu (0)\n");
-    }
-
-    if(Baralho[0].SuperPoder >= Baralho[1].SuperPoder){
-        printf("Super Poder: Carta 1 venceu (1)\n");
-    } else {
-        printf("Super Poder: Carta 2 venceu (0)\n");
-    }        
 
     printf("\nFinalizado com sucesso!\n");
 
